9703f31fa77f3c9a6395a125eac2650f6bd66e3e
Aravind <aravindm@nunify.com>
2025-12-19 18:21:03 +0530

    fix console issue

--- src/util/console.js
+++ src/util/console.js
@@ -2,7 +2,7 @@ function getConsole() {
   if (typeof window !== "undefined") {
     return window.console;
   }
-  return global.console;
+  return global ? global.console : console;
 }
 const console = getConsole();
 
